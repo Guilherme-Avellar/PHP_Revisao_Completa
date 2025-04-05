d71cf0bdece500611d12b8c8471d7548cdb9fe87
Guilherme <guilhermeavellar25@gmail.com>
2025-04-05 15:20:30 -0300

    arrays functions

--- sintaxe/arrays.php
+++ sintaxe/arrays.php
@@ -23,20 +23,42 @@ $arrayNomeado["idade"] = 40;
 $arrayNomeado2 = ["nome" => "Maria", "idade" => 39];
 //var_dump($arrayNomeado2);
 
-// count() é uma função nativa que retorna o número de elementos de um array
+
+// Funções para manipulação de arrays (essenciais e basicas):
+
+
+// count()
+// é uma função nativa que retorna o número de elementos de um array
 //echo "número de elementos do arrayNomeado2: ". count($arrayNomeado2);
 
 
 $numeros = ["um", "dois", "tres", "quatro"];
 
-// foreach() é uma função feita para percorrer arrays (pode ser usado em outras estruturas também):
-foreach($numeros as $numero){
+// foreach()
+// é uma função feita para percorrer arrays (pode ser usado em outras estruturas também):
+/*foreach($numeros as $numero){
     echo '$numero = '. $numero. "\n";
-}
+}*/
 // $numeros as $numero significa que:
 // Será chamado de $numero cada um dos elementos de $numeros, pois $numero
 // percorrerá cada um dos elementos de $numeros, como se fosse $numeros indexado
 
-// array_sum() é uma função nativa que retorna a soma de todos os elementos de um array
+
+// array_sum()
+// é uma função nativa que retorna a soma de todos os elementos de um array
 //echo "soma do arrayComum [2, 3, 4]  =  ". array_sum($arrayComum) ."\n";
+
+
+// sort()
+// é uma função que ordena um array
+$arrayDesordenado = [5, 2, 15, 3, 1, 5, -2];
+var_dump($arrayDesordenado);
+sort($arrayDesordenado);
+var_dump($arrayDesordenado);
+// obs: o parametro de sort() é uma referencia ao vetor passado (parecido com ponteiros do C)
+
+
+// min()
+// é uma função que retornará o menor valor do array
+echo "\n\nfunção min(): ". min($arrayDesordenado);
 
