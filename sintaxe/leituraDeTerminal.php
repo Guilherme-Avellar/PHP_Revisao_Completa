80655ac061294955758393b3639480d88738c3a6
Guilherme <guilhermeavellar25@gmail.com>
2025-04-05 11:52:21 -0300

    Read exemple

---
+++ sintaxe/leituraDeTerminal.php
@@ -0,0 +1,14 @@
+
+<?php
+
+    // Variavel (array) nativa do php que captura o que é digitado no terminal
+    $cpituraVariavel = $argv[1];
+
+    for($i = 1; $i < 5; $i++){
+        echo "\n\n$cpituraVariavel";
+    }
+
+    // Sujestão de terminal:  php .\leituraDeTerminal.php "ALGUMA COISA"
+
+
+
