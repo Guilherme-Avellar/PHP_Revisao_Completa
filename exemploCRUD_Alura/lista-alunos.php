bda4a78e907dd932c3f983ce9f7c661c5a2cab76
Guilherme <guilhermeavellar25@gmail.com>
2025-04-07 02:37:59 -0300

    exemple with while

--- exemploCRUD_Alura/lista-alunos.php
+++ exemploCRUD_Alura/lista-alunos.php
@@ -6,7 +6,8 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-// para se fazer uma query no banco usa-se o query() do obj PDO instanciado com o comando SQL que se quer:
+// para se fazer uma query no banco usa-se o query() do obj PDO instanciado com o comando SQL que se quer. Ou seja
+// guarda o comando SQL em uma variavel para ser usado depois
 $statement = $pdo->query('SELECT * FROM students;');
 // guardando em $studentDataList um array associativo dos estudantes, bem organizado para poder ser manipulado
 $studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -25,11 +26,37 @@ foreach ($studentDataList as $studentData) {
 
 //var_dump($studentList);
 
+
+
+
+
+
 // Para pegar apenas um único elemento do BD:
 $statementOne = $pdo->query('SELECT * FROM students WHERE id = 1;');
 // Usa-se o fetch(), já que você não precisará de uma lista de arrays com informações de cada linha da tabela
 $oneStudent = $statementOne->fetch(PDO::FETCH_ASSOC);
 //var_dump($oneStudent);
+
+// Uma estratégia para percorrer grandes volumes de dados é:
+/*
+while ($oneStudent = $statementOne->fetch(PDO::FETCH_ASSOC)) {
+    $student = new Student(
+        $oneStudent['id'],
+        $oneStudent['name'],
+        new \DateTimeImmutable($oneStudent['birth_date'])
+    );
+    // exemplo de operação, exibir a idade de todos os estudantes
+    echo $student->age() . PHP_EOL;
+}
+*/
+// por que esse while? Para não ocupar memória, ele pega a informação do BD, faz o que tem que ser feito dentro de 1
+// loop e joga o objeto fora na próxima interação com o loop, não ocupando memória com infinitos objetos, e usando 1 por
+// vez a cada loop.
+// E como sai do while? O PDO::FETCH_ASSOC que é o que a variavel $oneStudent esta utilizando, quando termina de
+// percorrer uma informação, ele coloca o "cursor" na próxima informação, e se for chamado novamente da mesma forma
+// ele lê apartir do "cursor" posto por ele mesmo, com o while executrando isso toda hora ele percorre o array inteiro,
+// caso não haja informação onde estiver o "cursor de leitura" dele, ele retorna false, que é o que sai do while.
+// Outros fetch funcionam da mesma forma, como o fetchColumn(), etc...
 
 
 
